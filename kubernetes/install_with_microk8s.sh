7f61158f2d46567e94b8081472818fcb567f96b6
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2024-05-16 02:05:49 +0530

    Add: new lines

--- kubernetes/install_with_microk8s.sh
+++ kubernetes/install_with_microk8s.sh
@@ -8,12 +8,14 @@ sudo usermod -a -G microk8s $USER
 mkdir -p ~/.kube
 chmod 0700 ~/.kube
 
+echo ""
 read -p "Do you want alias kubectl commands to k ? [Y/n]: " alias_k
 
 if [ "$alias_k" = "Y" ] || [ "$alias_k" = "y" ]; then
     sh docs/kubernetes/alias_k.sh
 fi
 
+echo ""
 read -p "Do you want reboot server? [Y/n]: " will_reboot
 
 if [ "$will_reboot" = "Y" ] || [ "$will_reboot" = "y" ]; then
