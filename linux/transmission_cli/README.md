015d8b616d09c512462ce70fec91459c084a993b
Yasitha Nadeeshan <yasithanadeeshan@gmail.com>
2024-05-16 20:31:45 +0530

    Add: termission cli

---
+++ linux/transmission_cli/README.md
@@ -0,0 +1,11 @@
+# Transmission CLI
+
+## Install
+```bash
+sh docs/linux/transmission_cli/install.sh
+```
+
+## Download
+```bash
+transmission-cli <path_of_torrent_file>
+```
---
+++ linux/transmission_cli/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:transmissionbt/ppa
+sudo apt-get update
+sudo apt-get install transmission-cli transmission-common transmission-daemon
