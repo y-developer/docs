45a1da43440b03d89aa977837e172b27e032275d
Yasitha Nadeeshan <64678612+Y-developer@users.noreply.github.com>
2024-10-02 20:20:21 +0530

    Update and rename alias_k.sh to alias.sh

--- kubernetes/alias.sh
+++ kubernetes/alias.sh
@@ -3,4 +3,3 @@
 echo "alias k=\"microk8s kubectl\"" >> $HOME/.bash_aliases
 echo "alias kubectl=\"microk8s kubectl\"" >> $HOME/.bash_aliases
 echo "alias helm=\"microk8s helm\"" >> $HOME/.bash_aliases
-exec bash
