26c2267aef1e924d8e76c9d2b9ff8f6f023ba9b1
Yasitha Nadeeshan <64678612+Y-developer@users.noreply.github.com>
2024-10-02 14:33:30 +0530

    Update alias_k.sh

--- kubernetes/alias_k.sh
+++ kubernetes/alias_k.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "alias k=\"microk8s kubectl\"" >> $HOME/.bash_aliases
+echo "alias kubectl=\"microk8s kubectl\"" >> $HOME/.bash_aliases
 echo "alias helm=\"microk8s helm\"" >> $HOME/.bash_aliases
 exec bash
