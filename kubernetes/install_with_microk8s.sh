eeb985c4651506f11655c8916e2e6774d1b1d6b0
Yasitha Nadeeshan <64678612+Y-developer@users.noreply.github.com>
2024-10-02 14:02:30 +0530

    Update install_with_microk8s.sh

--- kubernetes/install_with_microk8s.sh
+++ kubernetes/install_with_microk8s.sh
@@ -7,17 +7,3 @@ sudo snap install microk8s --classic --channel=1.30
 sudo usermod -a -G microk8s $USER
 mkdir -p ~/.kube
 chmod 0700 ~/.kube
-
-echo ""
-read -p "Do you want alias kubectl commands to k ? [Y/n]: " alias_k
-
-if [ "$alias_k" = "Y" ] || [ "$alias_k" = "y" ]; then
-    sh docs/kubernetes/alias_k.sh
-fi
-
-echo ""
-read -p "Do you want reboot server? [Y/n]: " will_reboot
-
-if [ "$will_reboot" = "Y" ] || [ "$will_reboot" = "y" ]; then
-    sudo reboot
-fi
